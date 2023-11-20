1d88650d97a281dd325f9a2e211526619d5bac0b
Luis Miguel López Magaña <luismi.lopez@triana.salesianos.edu>
2023-11-20 13:27:04 +0100

    Saludo a los alumnos de ED

---
+++ SaludoED/src/holamundo/SaludoEntornosDesarrollo.java
@@ -0,0 +1,10 @@
+package holamundo;
+
+public class SaludoEntornosDesarrollo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola a todos los alumnos de Entornos de Desarrollo");
+
+	}
+
+}
