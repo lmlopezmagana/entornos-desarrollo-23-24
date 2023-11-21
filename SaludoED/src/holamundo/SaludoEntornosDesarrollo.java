0c72334649bc426711a548fb788fa57badafc82b
Luis Miguel López Magaña <luismi.lopez@triana.salesianos.edu>
2023-11-21 14:05:15 +0100

    Se mejora el saludo a los alumnos

--- SaludoED/src/holamundo/SaludoEntornosDesarrollo.java
+++ SaludoED/src/holamundo/SaludoEntornosDesarrollo.java
@@ -3,8 +3,10 @@ package holamundo;
 public class SaludoEntornosDesarrollo {
 
 	public static void main(String[] args) {
+		System.out.println("-".repeat(50));
 		System.out.println("Hola a todos los alumnos de Entornos de Desarrollo");
-
+		System.out.println("-".repeat(50));
+			
 	}
 
 }
