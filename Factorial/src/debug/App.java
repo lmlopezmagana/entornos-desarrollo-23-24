b475718cc81ec2ddfbc3f8d77ebb543800ba2728
Luis Miguel López Magaña <luismi.lopez@triana.salesianos.edu>
2023-11-27 13:13:28 +0100

    Se modifica el ejemplo del factorial para trabajar con una clase.

--- Factorial/src/debug/App.java
+++ Factorial/src/debug/App.java
@@ -3,19 +3,11 @@ package debug;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("El factorial de 5 es "
-				+ factorial(5)
-				);
+		
 	}
 	
 	
-	public static int factorial(int n) {
-		int resultado = 1;
-		for(int i = n; i >= 1; i--) {
-			resultado = resultado * i;
-			//System.out.println(i);
-		}
-		return resultado;
-	}
-
+	
+	
+	
 }
