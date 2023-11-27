fea6ce617cf3ab5653418746c3987a3268fb9883
Luis Miguel López Magaña <luismi.lopez@triana.salesianos.edu>
2023-11-27 13:25:41 +0100

    Código con algún error

--- Factorial/src/debug/App.java
+++ Factorial/src/debug/App.java
@@ -3,7 +3,8 @@ package debug;
 public class App {
 
 	public static void main(String[] args) {
-		
+		MultiFactorial mf = new MultiFactorial(150);
+		mf.imprimeFactorial();
 	}
 	
 	
--- Factorial/src/debug/MultiFactorial.java
+++ Factorial/src/debug/MultiFactorial.java
@@ -4,39 +4,39 @@ import java.util.Random;
 
 public class MultiFactorial {
 	
-	private int[] elementos;
+	private double[] elementos;
 	
-	public MultiFactorial(int[] elem) {
+	public MultiFactorial(double[] elem) {
 		this.elementos = elem;
 	}
 	
 	public MultiFactorial(int n) {
-		this.elementos = new int[n];
+		this.elementos = new double[n];
 		Random r = new Random();
 		for(int i = 0; i < n; i++) {
-			this.elementos[i] = r.nextInt(10)+1;
+			this.elementos[i] = r.nextInt(100)+1;
 		}
 	}
 	
-	public static int factorial(int n) {
-		int resultado = 1;
-		for(int i = n; i >= 1; i--) {
+	public static double factorial(double n) {
+		double resultado = 1;
+		for(int i = (int) n; i >= 1; i--) {
 			resultado = resultado * i;
 		}
 		return resultado;
 	}
 	
 	public void imprimeFactorial() {
-		for(int i = 0; i < this.elementos.length; i++) {
+		for(int i = 0; i < this.elementos.length+1; i++) {
 			System.out
-			  .println("Factorial de %d: %d"
-					  .formatted(this.elementos[i],
+			  .println("Factorial de %.0f: %.0f"
+					  .formatted(this.elementos[i]-1,
 							  factorial(this.elementos[i])));
 		}
 	}
 	
-	public int[] calculaFactorial() {
-		int[] result = new int[this.elementos.length];
+	public double[] calculaFactorial() {
+		double[] result = new double[this.elementos.length];
 		for(int i = 0; i < this.elementos.length; i++) {
 			result[i] = factorial(this.elementos[i]);
 		}
